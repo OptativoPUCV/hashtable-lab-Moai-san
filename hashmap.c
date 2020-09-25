37a1ac0581b7c83c8f1a160fb4e54ff407539f65
repl.it user <replituser@example.com>
2020-09-25 17:29:38 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -70,7 +70,7 @@ void enlarge(HashMap * map) {
   Pair** a =(Pair**)calloc(map->capacity,sizeof(Pair*));
   map->capacity =((map->capacity)*2);  
   a =map->buckets;
-  a =((Pair**)realloc(a,2*sizeof(a)));
+  a =((Pair**)realloc(a,(2*sizeof(a))));
   map->buckets =a;
   //map->buckets = (Pair**)realloc(map->buckets,((map->capacity)*(sizeof(Pair*))));
 }
