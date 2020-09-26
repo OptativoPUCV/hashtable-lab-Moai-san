53a21d42cfb53fc2e4974cb41f4c878856e579e4
repl.it user <replituser@example.com>
2020-09-26 22:19:43 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -92,7 +92,7 @@ void enlarge(HashMap * map) {
   for (int i=0;i<map->capacity;i++)
   {
     test =(pivot[i]);
-    if ((test!=NULL)&&(test->key!=NULL))
+    if ((test!=NULL)&&(test->key!=NULL)&&(test->value!=NULL))
     {
       insertMapa(map,test->key,test->value);
     }
